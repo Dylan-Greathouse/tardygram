1c46742e7c2751cffb5cdbd16ef30b2401d49dc1
Alice-Retz <alicegretz@gmail.com>
2021-10-13 18:55:46 -0500

    auth/verify route test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,21 +3,31 @@ const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
 
+jest.mock('../lib/middleware/ensureAuth.js', () => {
+  return (req, res, next) => {
+    req.user = {
+      username: 'Dylan-Greathouse',
+      avatar: 'https://avatars.githubusercontent.com/u/20326640?v=4',
+      iat: Date.now(),
+      exp: Date.now(),
+    };
 
-jest.mock()
+    next();
+  };
+});
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('should return proper username and avatar', async () => {
-    const res = await request(app).get('/api/v1/auth/login/callback');
-    // const testUser = await User.insert(res.body);
-    // console.log('HELLLOOO', res.body);
+  it('should return user testing ensureAuth at /verify using mock', async () => {
+    const res = await request(app).get('/api/v1/auth/verify');
     expect(res.body).toEqual({
-      username: expect.any(String),
-      avatar: expect.any(String),
+      username: 'Dylan-Greathouse',
+      avatar: 'https://avatars.githubusercontent.com/u/20326640?v=4',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
     });
   });
 
