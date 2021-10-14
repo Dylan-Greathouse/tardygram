5fe7204246f77560b6af60a415366a1973f7c0ba
Joseph-K-B <303josephb@gmail.com>
2021-10-13 18:47:34 -0700

    pulling alice branch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,20 +4,30 @@ const request = require('supertest');
 const app = require('../lib/app.js');
 
 
-jest.mock()
+jest.mock('../lib/middleware/ensureAuth.js', () => {
+  return (req, res, next) => {
+    req.user = {
+      username: 'fake-github',
+      avatar: 'fake_image.png',
+      iat: Date.now(),
+      exp: Date.now()
+    };
+    next();
+  };
+});
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('should return proper username and avatar', async (res) => {
-    await request(app).get('/api/v1/auth/login');
-    // const testUser = await User.insert(res.body);
-    // console.log('HELLLOOO', res.body);
-    expect(res.user).toEqual({
+  it('should return proper username and avatar', async () => {
+    const res = await request(app).get('/api/v1/auth/verify');
+    expect(res.body).toEqual({
       username: expect.any(String),
       avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number)
     });
   });
 
